6b7875b37e29463a7dc9c0e6d63ffba21fc1bc8f
Brett Adams <brett@sepal.app>
2024-05-27 11:42:00 -0400

    listen on PORT

--- bases/app/src/sepal/app/server.clj
+++ bases/app/src/sepal/app/server.clj
@@ -2,9 +2,9 @@
   (:require [integrant.core :as ig]
             [ring.adapter.jetty9 :as jetty]))
 
-(defmethod ig/init-key ::jetty [_ {:keys [handler]}]
-  (jetty/run-jetty handler {:port 3000
-                            :join? false}))
+(defmethod ig/init-key ::jetty [_ {:keys [handler port join?]}]
+  (jetty/run-jetty handler {:port port
+                            :join? join?}))
 
 (defmethod ig/halt-key! ::jetty [_ server]
   (jetty/stop-server server))
