0bda73b9863d9244ade32da343ed2da4331aad5f
Brett Adams <brett@sepal.app>
2024-05-31 12:13:25 -0400

    create current aliases for wfo plantlist tables

---
+++ bases/migrations/resources/migrations/versions/20240531161228-alias-current-wfo-plant-list.up.sql
@@ -0,0 +1,12 @@
+create or replace view if not exists wfo_plantlist_current.name as
+select * from wfo_plantlist_2023_12.name;
+--;
+create or replace view wfo_plantlist_current.reference as
+select * from wfo_plantlist_2023_12.reference;
+--;
+create or replace view wfo_plantlist_current.synonym as
+select * from wfo_plantlist_2023_12.synonym;
+--;
+create or replace view wfo_plantlist_current.taxon as
+select * from wfo_plantlist_2023_12.taxon;
+--;
