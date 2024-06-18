3f7cf060fc4522a1fb2729613ecf158bea189b48
Brett Adams <brett@sepal.app>
2024-06-18 09:39:40 -0400

    fix dbmate parameters

--- projects/app/run.sh
+++ projects/app/run.sh
@@ -5,6 +5,6 @@ set -Eeuxo pipefail
 PGPORT=${PGPORT:-5432}
 DATABASE_URL="postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}"
 
-bin/dbmate migrate --url "$DATABASE_URL" --no-dump-schema && \
+bin/dbmate --url "$DATABASE_URL" --no-dump-schema migrate && \
 cd projects/app && \
 clojure -M:main
