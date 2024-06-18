e68e6797c3c616d2c2a1169eba19e480fb1c2df0
Brett Adams <brett@sepal.app>
2024-06-18 09:34:06 -0400

    use explicit path to dbmate in run.sh

--- projects/app/run.sh
+++ projects/app/run.sh
@@ -5,6 +5,6 @@ set -Eeuxo pipefail
 PGPORT=${PGPORT:-5432}
 DATABASE_URL="postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}"
 
-dbmate migrate --url "$DATABASE_URL" --no-dump-schema && \
+bin/dbmate migrate --url "$DATABASE_URL" --no-dump-schema && \
 cd projects/app && \
 clojure -M:main
