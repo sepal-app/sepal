e895100db427434c104c647991e450f066c7ab29
Brett Adams <brett@sepal.app>
2024-06-18 10:07:05 -0400

    use extra db connection params for cloud run

--- projects/app/run.sh
+++ projects/app/run.sh
@@ -3,7 +3,7 @@
 set -Eeuo pipefail
 
 PGPORT=${PGPORT:-5432}
-DATABASE_URL="postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}"
+DATABASE_URL="postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}:${PGPORT}/${PGDATABASE}?unixSocketPath=${DB_UNIX_SOCKET_PATH}&socketFactory=${DB_SOCKET_FACTORY}&cloudSqlInstance=${CLOUD_SQL_INSTANCE}"
 
 bin/dbmate --no-dump-schema migrate && \
 cd projects/app && \
