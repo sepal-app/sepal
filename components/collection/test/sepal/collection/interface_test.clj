4bf0b0b1fd1c99b9cc0913e8bda7f68073a9be91
Brett Adams <brett@sepal.app>
2025-11-09 07:21:00 -0500

    remove accidentally committed collection component

--- components/collection/test/sepal/collection/interface_test.clj
+++
@@ -1,7 +0,0 @@
-(ns sepal.collection.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [sepal.collection.interface :as collection]))
-
-(deftest dummy-test
-  (is (= 1 1)))
-
