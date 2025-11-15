0cf0fd75066195f9a3bb95c7370170ae7373e4d2
Brett Adams <brett@sepal.app>
2025-11-15 12:07:24 -0500

    use daisyui sidebar

--- bases/app/src/sepal/app/ui/base.clj
+++ bases/app/src/sepal/app/ui/base.clj
@@ -11,7 +11,9 @@
     [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
     [:style "[x-cloak] {display: none !important;}"]
     [:link {:rel "stylesheet"
-            :href (html/static-url "app/css/main.css")}]]
+            :href (html/static-url "app/css/main.css")}]
+    ;; See https://stackoverflow.com/questions/21147149/flash-of-unstyled-content-fouc-in-firefox-only-is-ff-slow-renderer
+    [:script "var PREVENT_FUOC_ON_FIREFOX"]]
    [:body {:hx-ext "alpine-morph"
            :hx-swap "morph"}
     content]])
--- bases/app/src/sepal/app/ui/page.clj
+++ bases/app/src/sepal/app/ui/page.clj
@@ -2,53 +2,125 @@
   (:require [sepal.app.flash :as flash]
             [sepal.app.globals :as g]
             [sepal.app.html :as html]
+            [sepal.app.routes.accession.routes :as accession.routes]
+            [sepal.app.routes.activity.routes :as activity.routes]
+            [sepal.app.routes.location.routes :as location.routes]
+            [sepal.app.routes.material.routes :as material.routes]
+            [sepal.app.routes.media.routes :as media.routes]
+            [sepal.app.routes.taxon.routes :as taxon.routes]
             [sepal.app.ui.base :as base]
+            [sepal.app.ui.icons.bootstrap :as bootstrap]
             [sepal.app.ui.icons.heroicons :as heroicons]
-            [sepal.app.ui.sidebar.mobile :as sidebar.mobile]
-            [sepal.app.ui.sidebar.static :as sidebar.static]))
+            [zodiac.core :as z]))
 
-(defn mobile-top-bar [& {:keys [title]}]
-  [:div {:class "sticky top-0 z-40 flex items-center gap-x-6 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-white shadow-sm"}
-   (sidebar.mobile/sidebar-button)
+(defn sidebar-item [& {:keys [href icon label] :as props}]
+  [:li
+   [:a
+    {:class "is-drawer-close:tooltip is-drawer-close:tooltip-right",
+     :href href
+     :data-tip label}
+    icon
+    [:span {:class "is-drawer-close:hidden"}
+     label]]])
 
-   [:div {:class "flex-1 text-sm font-semibold leading-6 text-gray-900"}
-    title]
+(defn sidebar-toggle-icon []
+  [:svg {:xmlns "http://www.w3.org/2000/svg",
+         :viewBox "0 0 24 24",
+         :stroke-linejoin "round",
+         :stroke-linecap "round",
+         :stroke-width "2",
+         :fill "none",
+         :stroke "currentColor",
+         :class "my-1.5 inline-block size-4"}
+   [:path {:d "M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 2 0 0 1 -2 -2z"}]
+   [:path {:d "M9 4v16"}]
+   [:path {:d "M14 10l2 2l-2 2"}]])
 
-   ;; Profile link
-   [:a {:href "#"}
-    [:span {:class "sr-only"} "Your profile"]
-    (if (:avatar-s3-key g/*viewer*)
-      [:img {:class "h-8 w-8 rounded-full"
-             :src "{{ current_user.avatar_url(50)|default('', true)}}"
-             :alt ""}]
-      (heroicons/user-circle :size 38))]])
+(defn page-wrapper [& {:keys [content flash footer] :as props}]
+  [:div {:class "drawer lg:drawer-open"}
+   [:input {:id "sidebar-drawer-toggle"
+            :type "checkbox"
+            :class "drawer-toggle"
+            ;; Setting checked makes the sidebar open by default. To have this follow page
+            ;; navigation we would probably need to store it in locale storage.
+            :checked "checked"}]
+   [:div {:class "drawer-content"}
+    ;; (comment "Navbar")
+    [:nav {:class "navbar w-full bg-base-100"}
+     [:div {:class "w-full flex flex-row justify-between items-center"}
+      [:label {:for "sidebar-drawer-toggle",
+               :aria-label "open sidebar",
+               :class "btn btn-square btn-ghost"}
+       ;; (comment "Sidebar toggle icon")
+       (sidebar-toggle-icon)]
 
-(defn page-wrapper [& {:keys [content flash footer]}]
-  [:div
-   [:div {:x-data "{showMobileSidebar: false}"
-          :x-cloak true}
-    [:div {:hx-boost "true"}
-     (sidebar.mobile/sidebar)
-     (sidebar.static/sidebar)]
-    [:div
-     [:div {:class "md:pl-64 flex flex-col flex-1"}
-      (mobile-top-bar)
+      #_[:a {:href "#"}
+         [:span {:class "sr-only"} "Your profile"]
+         (if (:avatar-s3-key g/*viewer*)
+           [:img {:class "h-8 w-8 rounded-full"
+                  :src "{{ current_user.avatar_url(50)|default('', true)}}"
+                  :alt ""}]
+           (heroicons/user-circle :size 38))]
+      ;; [:div "hi"]
+      ]
+     #_[:div {:class "px-4"} "Navbar Title"]]
+    ;; (comment "Page content here")
+    [:div ;; {:class "p-4"}
+     content
+     (flash/banner (:messages flash))
 
-      [:main {:id "page-main"
-              :class "flex-1"}
-       [:div {:class "py-6"}
-        [:div {:id "page-wrapper-content"
-               :class "max-w-7xl mx-auto px-4 sm:px-6 md:px-8"}
-         content]]]
+     (when footer
+       [:div {:id "page-footer"}
+        footer])
 
-      (flash/banner (:messages flash))
+     [:script {:type "module"
+               :src (html/static-url "app/ui/page.ts")}]]]
 
-      (when footer
-        [:div {:id "page-footer"}
-         footer])]]]
+   [:div {:class "drawer-side is-drawer-close:overflow-visible"}
+    [:label {:for "sidebar-drawer-toggle",
+             :aria-label "close sidebar",
+             :class "drawer-overlay"}]
+    [:div {:class
+           "flex min-h-full flex-col items-start bg-base-200 is-drawer-close:w-14 is-drawer-open:w-64"}
+     ;; (comment "Sidebar content here")
+     [:ul {:class "menu w-full grow flex flex-col justify-between"}
+      ;; (comment "List item")
+      [:div
+       (sidebar-item ;;"Activity"
+         :label "Activity"
+         :href (z/url-for activity.routes/index)
+         :icon (heroicons/outline-clock)
+         :current? false)
+       (sidebar-item :label "Accessions"
+                     :href (z/url-for accession.routes/index)
+                     :icon (heroicons/outline-rectangle-group)
+                     :current? false)
+       (sidebar-item :label "Material"
+                     :href (z/url-for material.routes/index)
+                     :icon (heroicons/outline-tag)
+                     :current? false)
+       (sidebar-item :label "Taxa"
+                     :href (z/url-for taxon.routes/index)
+                     :icon (bootstrap/flower1)
+                     :current? false)
+       (sidebar-item :label "Locations"
+                     :href (z/url-for location.routes/index)
+                     :icon (heroicons/outline-map-pin)
+                     :current? false)
+       (sidebar-item :label "Media"
+                     :href (z/url-for media.routes/index)
+                     :icon (heroicons/outline-photo)
+                     :current? false)]
 
-   [:script {:type "module"
-             :src (html/static-url "app/ui/page.ts")}]])
+      [:div {:class ""}
+       (sidebar-item :label "Profile"
+                     :href  "#"
+                     :icon (if (:avatar-s3-key g/*viewer*)
+                             [:img {:class "h-8 w-8 rounded-full"
+                                    :src "{{ current_user.avatar_url(50)|default('', true)}}"
+                                    :alt ""}]
+                             (heroicons/user-circle :size 38))
+                     :current? false)]]]]])
 
 (defn page [& {:keys [content flash footer page-title page-title-buttons attrs]}]
   (-> [:div (merge {} attrs)
