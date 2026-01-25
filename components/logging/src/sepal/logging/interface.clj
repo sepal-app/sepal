5503cf99cac843d77efb59a0641afc5c1c75cbb5
Brett Adams <brettadams@fastmail.com>
2026-01-24 20:55:15 -0500

    fix loggin, hopefully

--- components/logging/src/sepal/logging/interface.clj
+++ components/logging/src/sepal/logging/interface.clj
@@ -1,11 +1,12 @@
 (ns sepal.logging.interface
   (:require [clojure.string :as str]
             [integrant.core :as ig]
-            [taoensso.telemere] ;; setup logging side-effects
+            [taoensso.telemere :as tel]
             [taoensso.telemere.tools-logging :refer [tools-logging->telemere!]]))
 
 (defmethod ig/init-key ::logging [_ {:keys [level]}]
-  ;; Send clojure.tools.logging message to telemere
+  ;; Send clojure.tools.logging messages to telemere
   (tools-logging->telemere!)
-  (taoensso.telemere/set-min-level! :default "sepal.*"
-                                    (some-> level str/lower-case keyword)))
+  (when-let [lvl (some-> level str/lower-case keyword)]
+    ;; Set min level for all signal kinds (nil) in sepal.* namespaces
+    (tel/set-min-level! nil "sepal.*" lvl)))
