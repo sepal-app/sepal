c76847fc244457d209be227bef6cf786ea394900
Brett Adams <brettadams@fastmail.com>
2026-09-19 11:36:13 -0400

    Render the tag list through the shared table

--- bases/app/src/sepal/app/routes/tag/index.clj
+++ bases/app/src/sepal/app/routes/tag/index.clj
@@ -2,24 +2,39 @@
   (:require [sepal.app.routes.tag.routes :as tag.routes]
             [sepal.app.ui.empty :as ui.empty]
             [sepal.app.ui.page :as ui.page]
+            [sepal.app.ui.table :as ui.table]
             [sepal.tag.interface :as tag.i]
             [zodiac.core :as z]))
 
-(defn- row [tag]
-  [:tr
-   [:td [:a {:class "spl-link" :href (z/url-for tag.routes/detail {:id (:tag/id tag)})}
-         (:tag/name tag)]]
-   [:td (:tag/description tag)]
-   [:td (:tag/link-count tag)]])
+(defn- table-columns []
+  [{:name "Name"
+    :type :name
+    :priority 1
+    :stacked (fn [tag]
+               [:span {:class "spl-stacked-line"}
+                (:tag/description tag)
+                " · "
+                (:tag/link-count tag)
+                " links"])
+    :cell (fn [tag]
+            [:a {:class "spl-link"
+                 :href (z/url-for tag.routes/detail {:id (:tag/id tag)})}
+             (:tag/name tag)])}
+   {:name "Description"
+    :type :text
+    :priority 2
+    :cell :tag/description}
+   {:name "Links"
+    :type :number
+    :priority 2
+    :cell :tag/link-count}])
 
 (defn- table [tags]
-  (if (seq tags)
-    [:table {:class "spl-table"}
-     [:thead [:tr [:th "Name"] [:th "Description"] [:th "Links"]]]
-     [:tbody (for [tag tags] (row tag))]]
-    (ui.empty/empty-state
-      :title "No tags yet"
-      :body "Ad-hoc groupings a curator can filter a list by later.")))
+  (ui.table/table :columns (table-columns)
+                  :rows tags
+                  :empty-state (ui.empty/empty-state
+                                 :title "No tags yet"
+                                 :body "Ad-hoc groupings a curator can filter a list by later.")))
 
 (defn render [& {:keys [tags]}]
   (ui.page/page :content (table tags)
